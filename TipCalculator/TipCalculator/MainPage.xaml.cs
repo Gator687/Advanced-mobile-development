00f7e78d452044cf378fd658d3ad70a735459449
Gator687 <thomas.halligan68@gmail.com>
2023-08-01 01:29:37 -0400

    data bind and tip

---
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCalculateTipClicked(object sender, EventArgs e)
+        {
+            double bill = 0, tip = 0, percentage = 0;
+            try
+            {
+                bill = Convert.ToDouble(BillAmountEntry.Text);   
+            }
+            catch (Exception)
+            {
+                bill = 0;
+                BillAmountEntry.Focus();
+                return;
+            }
+
+            percentage = Convert.ToDouble(TipPercentagePicker.SelectedItem as string);
+
+            tip = bill * (percentage / 100.0);
+            TipAmountLabel.Text = tip.ToString("$0.##");
+        }
+    }
+}
---
+++ WeightConversion/WeightConversion/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WeightConversion;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
