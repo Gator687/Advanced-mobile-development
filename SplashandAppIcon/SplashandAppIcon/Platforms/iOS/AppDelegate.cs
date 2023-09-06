2918958182c00a62d21298efc912cd3ae9a5d024
Gator687 <thomas.halligan68@gmail.com>
2023-09-05 21:02:43 -0400

    splash and icon

---
+++ SplashandAppIcon/SplashandAppIcon/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SplashandAppIcon;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
